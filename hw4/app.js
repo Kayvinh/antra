992244e40e029a051dbf448164662404da2438e8
Kayvinh <kayvinhnguyen@gmail.com>
2022-05-25 18:25:05 -0700

    hw4 todolist

---
+++ hw4/app.js
@@ -0,0 +1,37 @@
+const baseUrl = 'https://jsonplaceholder.typicode.com'
+const path = 'todos'
+
+const getTodos = async () => {
+    const response = await fetch(`${baseUrl}/${path}`);
+    const data = await response.json();
+    // console.log(data);
+    return data;
+}
+
+const createTmp = (arr) => {
+    let tmp = "";
+    arr.forEach((item) => {
+        tmp += `
+        <li>
+            <div class="todo-item">
+                <span class="todo-content">${item.title}</span>
+            </div>
+          </li>
+        `
+    });
+    return tmp;
+}
+
+const render = (ele, tmp) => {
+    ele.innerHTML = tmp;
+};
+
+const printTodos = async () => {
+    const data = await getTodos();
+    const todoItems = document.querySelector('.todo-items');
+    const template = createTmp(data);
+    render(todoItems, template)
+
+}
+
+printTodos();
